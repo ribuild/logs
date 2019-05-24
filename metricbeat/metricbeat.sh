31a661203517d5eac779961929fad9d314420718
ocni <ocni@dtu.dk>
2019-05-24 14:15:18 +0000

    added startup file for metricbeat

---
+++ metricbeat/metricbeat.sh
@@ -0,0 +1,10 @@
+docker run -d \
+  --name=metricbeat \
+  --user=root \
+  --volume="$(pwd)/metricbeat.yml:/usr/share/metricbeat/metricbeat.yml:ro" \
+  --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
+  --volume="/sys/fs/cgroup:/hostfs/sys/fs/cgroup:ro" \
+  --volume="/proc:/hostfs/proc:ro" \
+  --volume="/:/hostfs:ro" \
+  docker.elastic.co/beats/metricbeat:7.1.0 metricbeat -e \
+  -E output.elasticsearch.hosts=["10.61.1.196:9200"]  
